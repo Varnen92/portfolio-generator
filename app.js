584c31b1ebccda08c04c3c754f46c550c2ff9c96
Robert Sandoval <robsand1992@yahoo.com>
2021-11-13 12:38:57 -0800

    added ability to generate index.html file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length)
+const fs = require('fs')
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i])
-    }
+const generatePage = require('./src/page-template.js')
+
+const profileDataArgs = process.argv.slice(2)
+
+const [name, github] = profileDataArgs
 
 
-console.log('====================')
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if (err) throw new Error(err);
 
-profileDataArr.forEach(profileItem => console.log(profileItem))
-};
-
-printProfileData(profileDataArgs)
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
